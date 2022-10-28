a68b14d432220fa34bd90182d28c8bfbc7833799
Divesh <Divesh@LAPTOP-QNTMPNLE>
2022-10-28 15:24:12 +0530

    submitted

---
+++ HelloWorld/src/OctoberTwo/This.java
@@ -0,0 +1,35 @@
+package OctoberTwo;
+
+
+class student{
+	int rollno;
+	String name;
+	float fee;
+	student (int rollno, String name, float fee)
+	{
+		this.rollno = rollno;
+		this.name = name;
+		this.fee = fee;
+	}
+	void display()
+	{
+		System.out.println(rollno+""+name+""+fee);
+	}
+
+}
+	
+public class This {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		student s1= new student (111,"aniket",5000f);
+		student s2 = new student (122,"Suraj",4000f);
+		student s3 = new student (123,"Dsk",5000f);
+    s1.display();
+    s2.display();
+    s3.display();
+	}
+
+}
+
+
