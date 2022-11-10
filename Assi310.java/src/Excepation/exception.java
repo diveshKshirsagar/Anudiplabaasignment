037740e71c31f77c5bda39af69c6f5c68d09f8c4
Divesh <Divesh@LAPTOP-QNTMPNLE>
2022-11-10 15:38:16 +0530

    Done

---
+++ Assi310.java/src/Excepation/exception.java
@@ -0,0 +1,11 @@
+package Excepation;
+
+class Zeromembererror  extends Exception  
+{  
+    public Zeromembererror (String str)  
+    {  
+        // calling the constructor of parent Exception  
+        super(str);  
+    }  
+}  
+    
